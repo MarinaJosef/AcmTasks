39d3f5f8857d91602ace6e20e3098d98e26b17eb
Marina <161172217+MarinaJosef@users.noreply.github.com>
2024-11-30 21:07:53 +0200

    Create LibraryUtils.ts

---
+++ LibraryUtils.ts
@@ -0,0 +1,20 @@
+import { Book } from "./Book";
+let this_id=1;
+
+export function generateID():number{
+    return this_id++;
+}
+export function validateBook(book:Book):Boolean{
+    let isValid:Boolean;
+
+    if (typeof book.id === 'number' &&
+    typeof book.title === 'string' &&
+    typeof book.author === 'string' &&
+    typeof book.isBorrowed === 'boolean' &&
+    typeof book.year === 'number' &&
+    (typeof book.borrowedBy === 'string' || book.borrowedBy === null) && typeof book.category === 'string')
+    isValid=true;
+    
+    else isValid=false;
+    return isValid;
+}
