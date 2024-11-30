768c84151ccd60f87b6022f8d14c05680c526d3c
Marina <161172217+MarinaJosef@users.noreply.github.com>
2024-11-30 21:03:58 +0200

    Create Book.ts

---
+++ Book.ts
@@ -0,0 +1,11 @@
+export type Book = {
+    id :number;
+    title:string;
+    author:string;
+    isBorrowed:boolean;
+    year:number;
+    borrowedBy:string|null;
+    category:string;
+  
+   }
+    
